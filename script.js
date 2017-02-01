e2d39e034e319594efb6b79f1ab549e73846d7b5
Nikolay Vyahhi <Nikolay.Vyahhi@stepik.org>
2017-02-01 18:50:56 -0500

    Add switcher to the top of wiki pages

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// http://stackoverflow.com/a/12646864/92396
 function shuffleArray(array) {
+	// http://stackoverflow.com/a/12646864/92396
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
@@ -9,33 +9,40 @@ function shuffleArray(array) {
     return array;
 }
 
-links = $('#mw-content-text p a').not($('.reference a'))
-words = new Set(links.toArray().map(link => link.text))
-words = Array.from(new Set(words)); // keep only unique
+function createQuizzes() {
+	links = $('#mw-content-text p a').not($('.reference a'))
+	words = new Set(links.toArray().map(link => link.text))
+	words = Array.from(new Set(words)); // keep only unique
 
-if (links.length >= 4) { // if less – we don't have enough options to choose for dropdowns
-	links.replaceWith(function() {
-		var correct = $(this).text();
-		var options = [correct];
-		while (options.length < 4) {
-			word = words[Math.floor(Math.random() * words.length)];
-			if (!options.includes(word)) {
-				options.push(word);
+	if (links.length >= 4) { // if less – we don't have enough options to choose for dropdowns
+		links.replaceWith(function() {
+			var correct = $(this).text();
+			var options = [correct];
+			while (options.length < 4) {
+				word = words[Math.floor(Math.random() * words.length)];
+				if (!options.includes(word)) {
+					options.push(word);
+				}
 			}
-		}
-		options = shuffleArray(options);
-		var select = '<select><option>' + options.join('</option><option>') + '</option></select>';
-		var item = $(select)
-		item.change(function(){
-			val = $(this).val()
-			if (val === correct) {
-				$(this).css('background-color', 'lightgreen');
-			}
-			else {
-				$(this).css('background-color', 'lightcoral');
-			}
+			options = shuffleArray(options);
+			var select = '<select><option>' + options.join('</option><option>') + '</option></select>';
+			var item = $(select)
+			item.change(function(){
+				val = $(this).val()
+				if (val === correct) {
+					$(this).css('background-color', 'lightgreen');
+				}
+				else {
+					$(this).css('background-color', 'lightcoral');
+				}
+			});
+			item.prop("selectedIndex", -1);
+			return item;
 		});
-		item.prop("selectedIndex", -1);
-		return item;
-	});
+	}
 }
+
+// TODO: wait here for jQuery to load
+
+switcher = $('<a>[TEST YOURSELF]</a>').click(createQuizzes).css('color', 'red');
+$('#firstHeading').append('&nbsp;').append(switcher);
