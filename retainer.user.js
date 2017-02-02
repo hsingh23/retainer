a4060b9b1dff24b4a7289c1b703f8597f2dc6bbe
Harsh Singh <harsingh@cimpress.com>
2017-02-01 21:19:37 -0500

    poc

--- retainer.user.js
+++ retainer.user.js
@@ -15,11 +15,11 @@
 function shuffleArray (array) {
     // http://stackoverflow.com/a/12646864/92396
   for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1))
-      var temp = array[i]
-      array[i] = array[j]
-      array[j] = temp
-    }
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
   return array
 }
 
@@ -37,35 +37,37 @@ function createQuizzes () {
   words = Array.from(new Set(words)) // keep only unique
 
   if (links.length >= 4) { // if less – we don't have enough options to choose for dropdowns
-      links.replaceWith(function () {
-          var correct = $(this).text()
-          var options = [correct]
-          while (options.length < 4) {
-              var word = words[Math.floor(Math.random() * words.length)]
-              if (!options.includes(word)) {
-                  options.push(word)
-                }
-            }
-          options = shuffleArray(options)
-          var select = '<select><option>' + options.join('</option><option>') + '</option></select>'
-          var item = $(select)
-          item.change(function () {
-              var val = $(this).val()
-              if (val === correct) {
-                  $(this).css('background-color', 'lightgreen')
-                }
-              else {
-                  $(this).css('background-color', 'lightcoral')
-                }
-            })
-          item.prop('selectedIndex', -1)
-          return item
-        })
-    }
+    links.replaceWith(function () {
+      var correct = $(this).text()
+      var options = [correct]
+      while (options.length < 4) {
+        var word = words[Math.floor(Math.random() * words.length)]
+        if (!options.includes(word)) {
+          options.push(word)
+        }
+      }
+      options = shuffleArray(options)
+      var select = '<select><option>' + options.join('</option><option>') + '</option></select>'
+      var item = $(select)
+      item.change(function () {
+        var val = $(this).val()
+        if (val === correct) {
+          $(this).css('background-color', 'lightgreen')
+        }
+        else {
+          $(this).css('background-color', 'lightcoral')
+        }
+      })
+      item.prop('selectedIndex', -1)
+      return item
+    })
+  }
 }
 
 addStyle('https://fonts.googleapis.com/icon?family=Material+Icons')
 addStyle('https://code.getmdl.io/1.3.0/material.orange-blue.min.css')
 
-var switcher = $('<a>[TEST YOURSELF]</a>').click(createQuizzes).css('color', 'red')
+var switcher = $(`<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+  <i class="material-icons">playlist_add_check</i>
+</button>`).click(createQuizzes).css('float', 'right')
 $('#firstHeading').append(' ').append(switcher)
