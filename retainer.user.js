f96112d208faffe7ab59a7deb1043b291f3d9ba5
Harsh Singh <harsingh@cimpress.com>
2017-02-01 19:43:59 -0500

    wip

--- retainer.user.js
+++ retainer.user.js
@@ -30,40 +30,40 @@ function addStyle (url) {
   document.querySelector('head').appendChild(link)
 }
 function createQuizzes () {
-	      links = $('#mw-content-text p a').not($('.reference a'))
-	      words = new Set(links.toArray().map(link => link.text))
-	      words = Array.from(new Set(words)) // keep only unique
+	          links = $('#mw-content-text p a').not($('.reference a'))
+	          words = new Set(links.toArray().map(link => link.text))
+	          words = Array.from(new Set(words)) // keep only unique
 
-	      if (links.length >= 4) { // if less – we don't have enough options to choose for dropdowns
-		      links.replaceWith(function () {
-			      var correct = $(this).text()
-			      var options = [correct]
-			      while (options.length < 4) {
-				      word = words[Math.floor(Math.random() * words.length)]
-				      if (!options.includes(word)) {
-					      options.push(word)
+	          if (links.length >= 4) { // if less – we don't have enough options to choose for dropdowns
+		          links.replaceWith(function () {
+			          var correct = $(this).text()
+			          var options = [correct]
+			          while (options.length < 4) {
+				          word = words[Math.floor(Math.random() * words.length)]
+				          if (!options.includes(word)) {
+					          options.push(word)
 				}
 			}
-			      options = shuffleArray(options)
-			      var select = '<select><option>' + options.join('</option><option>') + '</option></select>'
-			      var item = $(select)
-			      item.change(function () {
-				      val = $(this).val()
-				      if (val === correct) {
-					      $(this).css('background-color', 'lightgreen')
+			          options = shuffleArray(options)
+			          var select = '<select><option>' + options.join('</option><option>') + '</option></select>'
+			          var item = $(select)
+			          item.change(function () {
+				          val = $(this).val()
+				          if (val === correct) {
+					          $(this).css('background-color', 'lightgreen')
 				}
-				      else {
-					      $(this).css('background-color', 'lightcoral')
+				          else {
+					          $(this).css('background-color', 'lightcoral')
 				}
 			})
-			      item.prop('selectedIndex', -1)
-			      return item
+			          item.prop('selectedIndex', -1)
+			          return item
 		})
 	}
 }
 
 addStyle('https://fonts.googleapis.com/icon?family=Material+Icons')
-addStyle('https://code.getmdl.io/1.3.0/material.indigo-pink.min.css')
+addStyle('https://code.getmdl.io/1.3.0/material.orange-blue.min.css')
 
 switcher = $('<a>[TEST YOURSELF]</a>').click(createQuizzes).css('color', 'red')
 $('#firstHeading').append(' ').append(switcher)
